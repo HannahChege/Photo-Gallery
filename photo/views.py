36a5aa69e1cc8adcb833356f47aac13614586db5
HannahChege <chegehannah45@gmail.com>
2018-09-26 11:49:45 +0300

    Added the view function in the phot app

--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404# responsible for returning a response to a user
+import datetime as dt
+from .models import 
 
 # Create your views here.
+def welcome(request):
+    return HttpResponse('Fashion')
+
