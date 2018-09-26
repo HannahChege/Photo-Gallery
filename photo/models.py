91dd70f58195f4ce4afd3cdd2f330f8f2b95ea5b
HannahChege <chegehannah45@gmail.com>
2018-09-26 12:45:02 +0300

    Added the templates,photo,baseand navbarhtml

--- photo/models.py
+++ photo/models.py
@@ -1,9 +1,9 @@
-from django.db import models
-import datetime as dt
+# from django.db import models
+# import datetime as dt
 
-# Create your models here.
-class Image(models.Model):
-    name = models.CharField(max_length =30)
-    image = models.ImageField(upload_to ='')
-    description = models.CharField(max_length)
+# # Create your models here.
+# class Image(models.Model):
+#     name = models.CharField(max_length =30)
+#     image = models.ImageField(upload_to ='')
+#     description = models.CharField(max_length)
 
