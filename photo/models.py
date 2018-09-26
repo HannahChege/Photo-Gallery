237ca0e9a305c7ef982efe6f5fd1fedcb9ef8138
HannahChege <chegehannah45@gmail.com>
2018-09-27 00:33:04 +0300

    Added search function

--- photo/models.py
+++ photo/models.py
@@ -2,33 +2,48 @@ from django.db import models
 import datetime as dt
 
 # Create your models here.
+class Photographer(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    
+    def __str__(self):
+        return self.first_name
+    def save_photographer(self):
+        self.save()
+    def delete_photographer(self):
+        self.delete()   
+    @classmethod
+    def display_photographers(cls):
+        photographers= Photographer.objects.all()
+        for photographer in photographers:
+            return photographer        
+    class Meta:
+        ordering = ['first_name'] 
+
 class Image(models.Model):
-    name = models.CharField(max_length =30)
-    image = models.ImageField(blank=True, manual_crop='1600*1080')
+    title = models.CharField(max_length =30)
     description = models.TextField(max_length)
     image_location = models.ForeignKey(Location)
     image_category = models.ForeignKey( Category)
     def __str__(self):
-        return self.name
+        return self.title
     def save_image(self):
         self.save()
     def delete_image(self):
         self.delete()
-    @classmethod
-    def display_images(cls):
-        images= Image.object.all()
-        for image in images:
-            return image   
-    class Meta:
-        ordering = ['name']     
+   @classmethod
+    def search_by_title(cls,search_term):
+        photo = cls.objects.filter(title__icontains=search_term)
+        return photo        
+      
 class Location(models.Model):
     name = models.CharField(max_length =30)
-    description = models.TextField(max_length)
+
     def __str__(self):
         return self.name
 class Category(models.Model):
     name = models.CharField(max_length =30)
-    description = models.TextField(max_length)
+
     def __str__(self):
         return self.name        
 
--- photo/views.py
+++ photo/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404# responsible for returning a response to a user
 import datetime as dt
+from .models import Image,Location,Photographer,Category
 
 
 # Create your views here.
 def fashion(request):
-    return render(request, 'fashion.html')
+    return render(request, 'fashion.html',"photo")
+
+def search_results(request):
+    
+    if 'photo' in request.GET and request.GET["photo"]:
+        search_term = request.GET.get("photo")
+        searched_photos = Photo.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"photos": searched_photos})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
+
 
